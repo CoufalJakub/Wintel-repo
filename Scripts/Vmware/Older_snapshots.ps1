4588b0dbf0d2178182674e029c3563a01f58dea4
Jakub <39154966+Shazzye@users.noreply.github.com>
2018-05-29 10:21:41 +0200

    script

---
+++ Scripts/Vmware/Older_snapshots.ps1
@@ -0,0 +1,22 @@
+# Removes all snapshots with common name, that are older than x days
+Get-Module -Name VMware* -ListAvailable | Import-Module
+Connect-VIServer localhost
+
+
+$maxtasks = 5
+$days = 3
+$CommonName = "ASD"
+
+
+$snapshots = "*" + $CommonName + "*"
+$all_snapshots = Get-VM | Get-Snapshot | Where { $_.Name -like $snapshots -and $_.Created -lt (Get-Date).AddDays(-$days)}
+
+foreach($snapshot in $all_snapshots){
+        Remove-Snapshot -Snapshot $snapshot -RunAsync -Confirm:$false
+        $tasks = Get-Task -Status "Running" | where {$_.Name -eq "RemoveSnapshot_Task"}
+            while($tasks.Count -gt ($maxtasks-1)) {
+                sleep 30
+                $tasks = Get-Task -Status "Running" | where {$_.Name -eq "RemoveSnapshot_Task"}
+            }
+
+}
