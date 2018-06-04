c645fb9b22c6e51f0c2964a55f135a1848920b4f
Jakub <39154966+CoufalJakub@users.noreply.github.com>
2018-06-04 13:26:50 +0200

    Update and rename Remote_Install_WMIC.cmd to Remote_Install_WMIC.ps1

--- Scripts/Remote Install/Remote_Install_WMIC.ps1
+++ Scripts/Remote Install/Remote_Install_WMIC.ps1
@@ -1,3 +1,3 @@
-::Silent install on remote computer, needs source file on each computer
+#Silent install on remote computer, needs source file on each computer
 
 WMIC /NODE:@servers.txt /USER:"contoso\admin" /PASSWORD:"" PROCESS CALL CREATE "cmd.exe /c cd C:\temp & BES-Client.exe /S /v/qn /VINSTALLDIR="C:\IEM"" 
