41642c867d1bf8a379b9a61280b51149e4e0155f
Jakub <39154966+CoufalJakub@users.noreply.github.com>
2018-06-04 12:14:28 +0200

    Delete Start_AutoStopped.ps1

--- Scripts/Services/Start_AutoStopped.ps1
+++
@@ -1,9 +0,0 @@
-#Starts all stopped automatic services on computers from servers.txt
-
-$servers = gc servers.txt
-foreach($server in $servers){
-    Get-WmiObject Win32_Service -comp $server  | 
-        where{
-            ($_.state -like "*stopped*" -and $_.Startmode -like "*auto*")
-            }| start-service
-}
