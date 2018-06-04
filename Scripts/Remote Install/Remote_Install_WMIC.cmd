a469c9f0c64df768cfa7b873e061b38f048d1e7b
Jakub <39154966+CoufalJakub@users.noreply.github.com>
2018-06-04 13:26:26 +0200

    Update Remote_Install_WMIC.cmd

--- Scripts/Remote Install/Remote_Install_WMIC.cmd
+++ Scripts/Remote Install/Remote_Install_WMIC.cmd
@@ -1,7 +1,3 @@
 ::Silent install on remote computer, needs source file on each computer
 
-Set servers = GetObject(".\servers.txt")
-For Each server in servers
-	server = objComputer.CN
-	WMIC /NODE:"server" /USER:"contoso\admin" /PASSWORD:"" PROCESS CALL CREATE "cmd.exe /c cd C:\temp & BES-Client.exe /S /v/qn /VINSTALLDIR="C:\IEM"" 
-Next
+WMIC /NODE:@servers.txt /USER:"contoso\admin" /PASSWORD:"" PROCESS CALL CREATE "cmd.exe /c cd C:\temp & BES-Client.exe /S /v/qn /VINSTALLDIR="C:\IEM"" 
