65422ed251235c5f8ebae868a289bbd529e176a2
Jakub <39154966+CoufalJakub@users.noreply.github.com>
2018-06-04 09:45:47 +0200

    Update Start_AutoStopped.ps1

--- Scripts/Services/Start_AutoStopped.ps1
+++ Scripts/Services/Start_AutoStopped.ps1
@@ -1,8 +1,6 @@
 #Starts all stopped automatic services on computers from servers.txt
 
-
 $servers = gc servers.txt
-
 foreach($server in $servers){
     Get-WmiObject Win32_Service -comp $server  | 
         where{
