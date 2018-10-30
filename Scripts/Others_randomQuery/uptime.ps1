3ce13cc21652c854918e5bae8049b886e8fe8452
Jakub <39154966+CoufalJakub@users.noreply.github.com>
2018-10-30 09:05:28 +0100

    Update uptime.ps1

--- Scripts/Others_randomQuery/uptime.ps1
+++ Scripts/Others_randomQuery/uptime.ps1
@@ -1,14 +1,19 @@
 #Gather uptime of multiple servers
-$names = Get-Content "servers.txt" 
-@( 
-foreach ($name in $names){ 
-  if ( Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue ){ 
-    $wmi = gwmi Win32_OperatingSystem -computer $name 
-    $LBTime = $wmi.ConvertToDateTime($wmi.Lastbootuptime) 
-    [TimeSpan]$uptime = New-TimeSpan $LBTime $(get-date) 
-    Write-output "$name Uptime is  $($uptime.days) Days $($uptime.hours) Hours $($uptime.minutes) Minutes $($uptime.seconds) Seconds" 
-    } 
-  else{ 
-    Write-output "$name is not pinging" 
-  } 
-} ) | Out-file -FilePath "uptime_results.txt"
+
+        $names = Get-Content "servers.txt" 
+        @( 
+           foreach ($name in $names) 
+          { 
+            if ( Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue )  
+          { 
+            $wmi = gwmi Win32_OperatingSystem -computer $name 
+            $LBTime = $wmi.ConvertToDateTime($wmi.Lastbootuptime) 
+            [TimeSpan]$uptime = New-TimeSpan $LBTime $(get-date) 
+            Write-output "$name Uptime is  $($uptime.days) Days $($uptime.hours) Hours $($uptime.minutes) Minutes $($uptime.seconds) Seconds" 
+          } 
+             else { 
+                Write-output "$name is not pinging" 
+                  } 
+               } 
+         ) | Out-GridView -Title 'Uptime'
+pause
