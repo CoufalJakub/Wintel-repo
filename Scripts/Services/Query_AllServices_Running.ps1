139bd863145dac82a7c25b62f1ded3c1a23803a7
Jakub <39154966+CoufalJakub@users.noreply.github.com>
2019-01-04 13:12:21 +0100

    Update Query_AllServices_Running.ps1

--- Scripts/Services/Query_AllServices_Running.ps1
+++ Scripts/Services/Query_AllServices_Running.ps1
@@ -4,7 +4,5 @@ $serverlist = gc servers.txt
 foreach($server in $serverlist){
     write-output $server | out-file services.txt -append
     Get-WmiObject Win32_Service -comp $server  | 
-        where{
-            ($_.state -like "*running*")
-            }|select DisplayName,Name,StartMode,State|ft -AutoSize | out-file services.txt -append
+        where{($_.state -like "*running*")}|select DisplayName,Name,StartMode,State|ft -AutoSize | out-file services.txt -append
 }
